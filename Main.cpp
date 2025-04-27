1844eb2cb7a1035bfededbd5cfd0c1189bb6fe01
Arianna <87552392+ar14nn422@users.noreply.github.com>
2025-04-27 12:02:11 -0600

    Update Main.cpp
    Info

--- Main.cpp
+++ Main.cpp
@@ -5,3 +5,23 @@ int main() {
     interfaz.mostrarMenuPrincipal();
     return 0;
 }
+
+
+
+/* -------------------------------------------------------------------+
+ *                                                                    |
+ *(c)2025                                                             |
+ *EIF204-Programación 2                                               |
+ *1er ciclo 2025                                                      |
+ *NRC41370 -Grupo05                                                   | 
+ *Proyecto 1                                                          |
+ *                                                                    |
+ *208660814; Arianna Barquero Ortiz                                   | 
+ *208280313; Brandon Bejarano Moya                                    |
+ *123950544; Sebastián Castro Guevara                                 |
+ *119370141; Yoel Hernández Zamora                                    |
+ *                                                                    |
+ *versión 1.0.0  27-04-2025                                           |
+ *                                                                    |
+ * -------------------------------------------------------------------+
+ */
